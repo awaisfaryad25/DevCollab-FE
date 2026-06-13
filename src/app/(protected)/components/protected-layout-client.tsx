0d7df7dbe783d69ded1cc1a59f9db882c2e0247c
M Awais Ch <awaisfaryad25@gmail.com>
2026-06-13 23:00:34 +0500

    basic csss style

--- src/app/(protected)/components/protected-layout-client.tsx
+++ src/app/(protected)/components/protected-layout-client.tsx
@@ -23,7 +23,7 @@ export function ProtectedLayoutClient({
 
         {/* Navbar */}
         <header className="sticky top-0 z-50 flex h-16 items-center justify-between border-b bg-sidebar px-4">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center justify-between gap-3">
             <ModeToggle />
             <UserNav />
           </div>
